a4014d2cdfc84ad57797bb1283d0dc22428d3e3a
Vince Muller <117208947+vincemuller@users.noreply.github.com>
2023-12-21 07:37:39 -0700

    Updating Error enum to account for invalid response (#1)
    Squash merging from dev. branch

--- CSID_App/utilities/ErrorMessage.swift
+++ CSID_App/utilities/ErrorMessage.swift
@@ -10,4 +10,5 @@ import Foundation
 enum CAError: String {
     case invalidUserID      = "Unable to retrieve iCloud details, please make sure you are signed into your iCloud account and try again."
     case unableToRemove     = "This item is still being saved to your favorites, please try again"
+    case invalidResponse    = "Invalid response from the server. Please try again."
 }
